6927a7887a0e3b10e61a380e727cbf5b1cd06d23
emagdnimMQT <mark.w.novak@gmail.com>
2022-06-19 11:54:56 -0400

    Updated slick function.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
     $('.slider-ctr').slick({
         slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev')
     });
   });
